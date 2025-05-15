c5c8f06f5d2b8c58ff7fe584e5ba9de103f228fd
wakana <wknwknwkn911@gmail.com>
2025-05-15 16:40:32 +0900

    userテーブルのidをuuidに変更

---
+++ prisma/migrations/20250515072901_userfix/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - The primary key for the `user` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "post" DROP CONSTRAINT "post_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "post" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "user" DROP CONSTRAINT "user_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "user_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "user_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "post" ADD CONSTRAINT "post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
