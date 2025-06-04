26f183c37cff0fa120e96d3860821a11f6cc212b
wakana <wknwknwkn911@gmail.com>
2025-06-04 16:55:52 +0900

    フッター修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Footer from "@/components/Footer";
 import SigninPage from "./signin/page";
 
 export default function Home() {
   return (
     <div>
       <SigninPage/>
-      <Footer/>
     </div>
   );
 }
