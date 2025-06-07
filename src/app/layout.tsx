411f3e77005e7bb27a647b240ee811dfd831ddd0
wakana <wknwknwkn911@gmail.com>
2025-06-07 14:40:59 +0900

    フッター固定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,11 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
+        <main className="flex-grow">
         {children}
+        </main>
         <Footer/>
       </body>
     </html>
