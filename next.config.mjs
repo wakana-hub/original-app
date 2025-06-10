6bf1fae44e03071e4b3fe6754e5782ab5fde2c54
wakana <wknwknwkn911@gmail.com>
2025-06-10 15:22:34 +0900

    mjs形式に修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     eslint: {
-    ignoreDuringBuilds: true, 
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
 
