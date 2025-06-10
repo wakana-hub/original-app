3d9a682c2f7f753795b016063d9165561631b885
wakana-hub <wknwknwkn911@gmail.com>
2025-06-10 12:34:04 +0900

    .ts⇒.jsに修正 (#31)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,4 +16,3 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
